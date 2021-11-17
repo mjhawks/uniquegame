c9eb7a31778e7f6d45a8021510bb5e21b9c0b133
Mason <mjhawks721@gmail.com>
2021-11-17 11:08:14 -0600

    cleaned up adding new board

--- game.js
+++ game.js
@@ -6,7 +6,11 @@ function generate()
     {
         if (confirm("this will clear the current game, are you sure?"))
         {
-            drawGameBoard();
+            deleteGameBoard();
+            setTimeout(function(){
+                drawGameBoard();
+                }, 3400);
+            
         }
     }    
     else
@@ -20,6 +24,7 @@ function drawGameBoard()
 {
     const body = document.body,
           tbl = document.createElement('table');
+    tbl.id = 'gameBoard';
     tbl.style.marginLeft = 'auto';
     tbl.style.marginRight = 'auto';
     for (let i = 0; i<10; i++){
@@ -28,10 +33,26 @@ function drawGameBoard()
         {
             setTimeout(function(){
             const td = tr.insertCell();
-            td.appendChild(document.createTextNode(``));
+            td.appendChild(document.createTextNode(''));
+            td.id = `${i} ${j}`;
             }, (j+i)*120);
             
         }
     }
     body.appendChild(tbl);
+}
+function deleteGameBoard()
+{
+    var board = document.getElementById("gameBoard");
+    for (let i = 0; i<10; i++){
+        for (let j = 0; j<10; j++)
+        {
+            setTimeout(function(){
+            document.getElementById(`${i} ${j}`).id = "removeanimation";
+            }, (j+i)*120);
+        }
+    }
+    setTimeout(function(){
+    board.remove();
+    }, 3400);
 }
